daf81f2672da93770e2472ed83a40de94eccc98e
Layla <54958879+APieceOfChalk@users.noreply.github.com>
2022-04-07 02:00:21 +0300

    Лаба 3

---
+++ Currency/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require('cors')
+const axios = require("axios");
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.listen(8082,() => {
+    console.log("Started on PORT 8082");
+})
+
+app.get('/test', (req, res) => {
+    res.end("OK")
+});
+
+app.post('/getExchangeRateByCurrencyName', async (req, res) => {
+    let exchangeRate = "";
+
+    await axios.get("https://www.cbr-xml-daily.ru/daily_json.js")
+        .then(response => {
+            const data = response.data;
+            exchangeRate = data["Valute"][req.body.currency]["Value"];
+            console.log(exchangeRate)
+        })
+        .catch();
+
+    res.end(JSON.stringify({exchangeRate: exchangeRate}))
+});
